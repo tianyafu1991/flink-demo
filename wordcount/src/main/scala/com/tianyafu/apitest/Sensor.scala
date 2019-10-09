cbd5f5307cea80cbd9d740c4377b6fd8adf2759f
tianyafu <1193835093@qq.com>
2019-10-09 23:40:45 +0800

    feat:添加 source api中的从文件中读取数据的demo

--- wordcount/src/main/scala/com/tianyafu/apitest/Sensor.scala
+++ wordcount/src/main/scala/com/tianyafu/apitest/Sensor.scala
@@ -12,6 +12,7 @@ object Sensor {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
     //2.source
+    // source1 ： 从集合中读取数据
     val stream1 = env.fromCollection(List(
       SensorReading("sensor_1",1547718199,35.80018327300259),
       SensorReading("sensor_6",1547718201,15.402984393403084),
@@ -19,11 +20,15 @@ object Sensor {
       SensorReading("sensor_10",1547718205,38.101067604893444)
     ))
 
+    //source2 : 从文件中读取数据
+    val inputPath = "E:\\WorkSpace\\IDEAWorkspace\\flinkdemo\\wordcount\\src\\main\\resource\\sensor.txt"
+    val stream2 = env.readTextFile(inputPath)
     //tranformation
 
 
     //sink
     stream1.print("stream1").setParallelism(1)
+    stream2.print("stream2").setParallelism(1)
 
     env.execute("api test")
 
