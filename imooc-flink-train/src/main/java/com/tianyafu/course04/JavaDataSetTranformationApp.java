f88a79468c7fb32cc6431b6c074035b6c9d97acc
tianyafu <futy@huawangtech.com>
2019-10-28 21:32:24 +0800

    feat:添加了tranformation 的scala和java api

--- imooc-flink-train/src/main/java/com/tianyafu/course04/JavaDataSetTranformationApp.java
+++ imooc-flink-train/src/main/java/com/tianyafu/course04/JavaDataSetTranformationApp.java
@@ -1,13 +1,11 @@
 package com.tianyafu.course04;
 
-import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.functions.MapFunction;
-import org.apache.flink.api.common.functions.MapPartitionFunction;
+import org.apache.flink.api.common.functions.*;
+import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.DataSource;
-import org.apache.flink.api.java.operators.FilterOperator;
-import org.apache.flink.api.java.operators.MapOperator;
-import org.apache.flink.api.java.operators.MapPartitionOperator;
+import org.apache.flink.api.java.operators.*;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.util.Collector;
 
 import java.util.ArrayList;
@@ -20,7 +18,199 @@ public class JavaDataSetTranformationApp {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 //        mapFunction(env);
 //        filterFunction(env);
-        mapPartitionFunction(env);
+//        mapPartitionFunction(env);
+//        FirstFunction(env);
+//        flatMapFunction(env);
+//        distinctFunction(env);
+//        joinFunction(env);
+//        outerJoinFunction(env);
+        crossFunction(env);
+    }
+
+    /**
+     * 笛卡尔积
+     * @param env
+     * @throws Exception
+     */
+    public static void crossFunction(ExecutionEnvironment env) throws Exception {
+        List<String> list1 = new ArrayList<>();
+        list1.add("曼联");
+        list1.add("曼城");
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(1);
+        list2.add(0);
+
+        DataSource<String> data1 = env.fromCollection(list1);
+        DataSource<Integer> data2 = env.fromCollection(list2);
+
+        data1.cross(data2).print();
+    }
+
+    public static void outerJoinFunction(ExecutionEnvironment env) throws Exception {
+        List<Tuple2<Integer,String>> info1 = new ArrayList<>();
+        info1.add(new Tuple2(1,"PK哥"));
+        info1.add(new Tuple2(2,"南哥"));
+        info1.add(new Tuple2(3,"香蕉哥"));
+        info1.add(new Tuple2(4,"猪头呼"));
+
+        List<Tuple2<Integer,String>> info2 = new ArrayList<>();
+        info2.add(new Tuple2(1,"北京"));
+        info2.add(new Tuple2(2,"上海"));
+        info2.add(new Tuple2(3,"成都"));
+        info2.add(new Tuple2(5,"杭州"));
+
+        DataSource<Tuple2<Integer, String>> data1 = env.fromCollection(info1);
+        DataSource<Tuple2<Integer, String>> data2 = env.fromCollection(info2);
+
+        //左外连接
+        /*data1.leftOuterJoin(data2).where(0).equalTo(0).with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer,String,String>>() {
+            @Override
+            public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                if (second==null) {
+                    return new Tuple3<>(first.f0,first.f1,"-");
+                }else {
+                    return new Tuple3<>(first.f0,first.f1,second.f1);
+                }
+            }
+        }).print();*/
+
+        //右外连接
+        /*data1.rightOuterJoin(data2).where(0).equalTo(0).with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer,String,String>>() {
+            @Override
+            public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                if (first==null) {
+                    return new Tuple3<>(second.f0,"-",second.f1);
+                }else {
+                    return new Tuple3<>(first.f0,first.f1,second.f1);
+                }
+            }
+        }).print();*/
+
+        //全外连接
+
+        data1.fullOuterJoin(data2).where(0).equalTo(0).with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer,String,String>>() {
+            @Override
+            public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                if (first==null) {
+                    return new Tuple3<>(second.f0,"-",second.f1);
+                }else if (second==null){
+                    return new Tuple3<>(first.f0,first.f1,"-");
+                }else {
+                    return new Tuple3<>(first.f0,first.f1,second.f1);
+                }
+            }
+        }).print();
+    }
+
+    /**
+     * 内连接
+     * @param env
+     * @throws Exception
+     */
+    public static void joinFunction(ExecutionEnvironment env) throws Exception {
+        List<Tuple2<Integer,String>> info1 = new ArrayList<>();
+        info1.add(new Tuple2(1,"PK哥"));
+        info1.add(new Tuple2(2,"南哥"));
+        info1.add(new Tuple2(3,"香蕉哥"));
+        info1.add(new Tuple2(4,"猪头呼"));
+
+        List<Tuple2<Integer,String>> info2 = new ArrayList<>();
+        info2.add(new Tuple2(1,"北京"));
+        info2.add(new Tuple2(2,"上海"));
+        info2.add(new Tuple2(3,"成都"));
+        info2.add(new Tuple2(5,"杭州"));
+
+        DataSource<Tuple2<Integer, String>> data1 = env.fromCollection(info1);
+        DataSource<Tuple2<Integer, String>> data2 = env.fromCollection(info2);
+
+        data1.join(data2).where(0).equalTo(0).with(new JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer,String,String>>() {
+            @Override
+            public Tuple3<Integer, String, String> join(Tuple2<Integer, String> first, Tuple2<Integer, String> second) throws Exception {
+                return new Tuple3<>(first.f0,first.f1,second.f1);
+            }
+        }).print();
+    }
+
+
+    /**
+     * distinct算子
+     * @param env
+     * @throws Exception
+     */
+    public static void distinctFunction(ExecutionEnvironment env) throws Exception {
+        List<String> lists = new ArrayList<>();
+        lists.add("hello spark");
+        lists.add("flink spark");
+        lists.add("elasticsearch spark");
+        lists.add("hello flink");
+
+        DataSource<String> dataSource = env.fromCollection(lists);
+
+        dataSource.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String input, Collector<String> collector) throws Exception {
+                String[] splits = input.split(" ");
+                for (String split : splits) {
+                    collector.collect(split);
+                }
+            }
+        }).distinct().print();
+    }
+
+    public static void flatMapFunction(ExecutionEnvironment env)throws Exception{
+        List<String> lists = new ArrayList<>();
+        lists.add("hello spark");
+        lists.add("flink spark");
+        lists.add("elasticsearch spark");
+        lists.add("hello flink");
+
+        DataSource<String> dataSource = env.fromCollection(lists);
+
+        AggregateOperator<Tuple2<String, Integer>> sum = dataSource.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String input, Collector<String> collector) throws Exception {
+                String[] splits = input.split(" ");
+                for (String split : splits) {
+                    collector.collect(split);
+                }
+            }
+        }).map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String input) throws Exception {
+                return new Tuple2<>(input, 1);
+            }
+        }).groupBy(0).sum(1);
+
+        sum.print();
+    }
+
+
+    /**
+     * First方法
+     * @param env
+     * @throws Exception
+     */
+    public static void FirstFunction(ExecutionEnvironment env) throws Exception {
+        List<Tuple2<Integer,String>> data = new ArrayList<>();
+        data.add(new Tuple2(1,"Flink"));
+        data.add(new Tuple2(1,"hadoop"));
+        data.add(new Tuple2(1,"spark"));
+        data.add(new Tuple2(2,"java"));
+        data.add(new Tuple2(2,"springboot"));
+        data.add(new Tuple2(3,"linux"));
+        data.add(new Tuple2(4,"vue"));
+
+        DataSource<Tuple2<Integer, String>> dataSource = env.fromCollection(data);
+
+        GroupReduceOperator<Tuple2<Integer, String>, Tuple2<Integer, String>> firstOperator = dataSource.first(2);
+        firstOperator.print();
+
+        GroupReduceOperator<Tuple2<Integer, String>, Tuple2<Integer, String>> firstGroupOperator = dataSource.groupBy(0).first(2);
+        firstGroupOperator.print();
+
+        GroupReduceOperator<Tuple2<Integer, String>, Tuple2<Integer, String>> firstGroupSortOperator = dataSource.groupBy(0).sortGroup(1, Order.DESCENDING).first(2);
+        firstGroupSortOperator.print();
 
     }
 
--- imooc-flink-train/src/main/scala/com/tianyafu/course04/DataSetTranformationApp.scala
+++ imooc-flink-train/src/main/scala/com/tianyafu/course04/DataSetTranformationApp.scala
@@ -10,7 +10,99 @@ object DataSetTranformationApp {
   def main(args: Array[String]): Unit = {
 
     val env = ExecutionEnvironment.getExecutionEnvironment
-    firstFunction(env)
+//    firstFunction(env)
+//    joinFunction(env)
+//    outerJoinFunction(env)
+    crossFunction(env)
+  }
+
+  /**
+    * 笛卡尔积
+    * @param env
+    */
+  def crossFunction(env:ExecutionEnvironment): Unit ={
+    val list1 = List("曼联","曼城")
+    val list2 = List(3,1,0)
+
+    val data1 = env.fromCollection(list1)
+    val data2 = env.fromCollection(list2)
+
+    data1.cross(data2).print()
+  }
+
+  /**
+    * 外连接
+    * @param env
+    */
+  def outerJoinFunction(env:ExecutionEnvironment): Unit ={
+    val info1  = ListBuffer[(Int,String)]()
+    info1.append((1,"PK哥"))
+    info1.append((2,"J哥"))
+    info1.append((3,"香蕉哥"))
+    info1.append((4,"猪头呼"))
+
+    val info2 = ListBuffer[(Int,String)]()
+    info2.append((1,"北京"))
+    info2.append((2,"上海"))
+    info2.append((3,"成都"))
+    info2.append((5,"杭州"))
+
+    val data1 = env.fromCollection(info1)
+    val data2 = env.fromCollection(info2)
+
+    //左外连接
+   /* data1.leftOuterJoin(data2).where(0).equalTo(0).apply(
+      (first,second)=>{
+      if(second ==null){
+        (first._1,first._2,"-")
+      }else{
+        (first._1,first._2,second._2)
+      }
+    }).print()*/
+
+    //右外连接
+    /*data1.rightOuterJoin(data2).where(0).equalTo(0).apply((first,second)=>{
+      if(first==null){
+        (second._1,"-",second._2)
+      }else{
+        (first._1,first._2,second._2)
+      }
+    }).print()*/
+
+    //全外连接
+    data1.fullOuterJoin(data2).where(0).equalTo(0).apply((first,second)=>{
+      if(second==null){
+        (first._1,first._2,"-")
+      }else if (first==null){
+        (second._1,"-",second._2)
+      }else{
+        (first._1,first._2,second._2)
+      }
+
+    }).print()
+  }
+
+  /**
+    * 内连接
+    * @param env
+    */
+  def joinFunction(env:ExecutionEnvironment): Unit ={
+    val info1  = ListBuffer[(Int,String)]()
+    info1.append((1,"PK哥"))
+    info1.append((2,"J哥"))
+    info1.append((3,"香蕉哥"))
+    info1.append((4,"猪头呼"))
+
+    val info2 = ListBuffer[(Int,String)]()
+    info2.append((1,"北京"))
+    info2.append((2,"上海"))
+    info2.append((3,"成都"))
+    info2.append((5,"杭州"))
+
+    val data1 = env.fromCollection(info1)
+    val data2 = env.fromCollection(info2)
+
+    data1.join(data2).where(0).equalTo(0).apply((first,second)=>(first._1,first._2,second._2)).print()
 
   }
 
