8f4c6175d1d54f6f5acb486cce618d110ad1c5b6
tianyafu <futy@huawangtech.com>
2019-11-09 10:46:16 +0800

    feat:增加ds-sell module 和zhisheng课程学习笔记

---
+++ ds-sell/src/main/java/com/imooc/dsservice/DsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.dsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsServiceApplication.class, args);
+    }
+
+}
